57ac1294dcbd8f2f28955dbbf11a3d08a5461630
Elena Ivanova <elena_ivanova@epam.com>
2023-03-22 10:55:53 +0300

    Added creating file with mocked data if required

--- lib/dataHelpers/getDbUsers.ts
+++ lib/dataHelpers/getDbUsers.ts
@@ -1,8 +1,16 @@
-import { readFileSync } from "fs";
+import { access, constants, readFileSync, writeFileSync } from "fs";
 
+import { mockedData } from "../../utils/constants";
 import { UsersDataList } from "../../utils/types";
 
 export function getDbUsers(): UsersDataList {
+  access("mockedDbData.json", constants.F_OK, (err) => {
+    if (err) {
+      console.log("no file");
+      const newDataJson = JSON.stringify(mockedData, null, " ");
+      writeFileSync("mockedDbData.json", newDataJson);
+    }
+  });
   const data = readFileSync("mockedDbData.json", "utf8");
   const { users } = JSON.parse(data);
   return users;
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,51 @@
-import { Contact } from "./types";
+import { Contact, DbData } from "./types";
+
+const mockedData: DbData = {
+  users: [
+    {
+      id: "1",
+      name: "Mary Doe",
+      email: "1",
+      password: "1",
+    },
+  ],
+  usersContacts: [
+    {
+      userId: "1",
+      contacts: [],
+    },
+  ],
+  commonContacts: [
+    {
+      id: 1,
+      name: "Fire",
+      phone: "01",
+      address: "",
+      email: "",
+    },
+    {
+      id: 2,
+      name: "Police",
+      phone: "02",
+      address: "",
+      email: "",
+    },
+    {
+      id: 3,
+      name: "Ambulance",
+      phone: "03",
+      address: "",
+      email: "",
+    },
+    {
+      id: 4,
+      name: "Ambulance",
+      phone: "03",
+      address: "",
+      email: "",
+    },
+  ],
+};
 
 enum ModalVariants {
   DELETE_CONTACT,
@@ -13,4 +60,4 @@ const emptyContact: Contact = {
   email: "",
 };
 
-export { ModalVariants, emptyContact };
+export { ModalVariants, emptyContact, mockedData };
