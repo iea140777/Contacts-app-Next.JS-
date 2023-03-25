db800d7cf5fc5ff60a0af8beb2571bfd056bf598
Elena Ivanova <elena_ivanova@epam.com>
2023-03-25 13:03:46 +0300

    fixed creating new file

--- lib/dataHelpers/getDbUsers.ts
+++ lib/dataHelpers/getDbUsers.ts
@@ -1,17 +1,16 @@
-import { access, constants, readFileSync, writeFileSync } from "fs";
+import { existsSync, readFileSync, writeFileSync } from "fs";
 
 import { mockedData } from "../../utils/constants";
 import { UsersDataList } from "../../utils/types";
 
 export function getDbUsers(): UsersDataList {
-  access("mockedDbData.json", constants.F_OK, (err) => {
-    if (err) {
-      console.log("no file");
-      const newDataJson = JSON.stringify(mockedData, null, " ");
-      writeFileSync("mockedDbData.json", newDataJson);
-    }
-  });
+  if (!existsSync("mockedDbData.json")) {
+    const newDataJson = JSON.stringify(mockedData, null, " ");
+    writeFileSync("mockedDbData.json", newDataJson);
+  }
+
   const data = readFileSync("mockedDbData.json", "utf8");
+
   const { users } = JSON.parse(data);
   return users;
 }
