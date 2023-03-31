e1b2afbd862f55006ff47a5565cb76adebbb4ae3
Elena Ivanova <elena_ivanova@epam.com>
2023-03-31 19:12:53 +0300

    fixed paths for data file

--- lib/dataHelpers/getDbUsers.ts
+++ lib/dataHelpers/getDbUsers.ts
@@ -4,12 +4,12 @@ import { mockedData } from "../../utils/constants";
 import { UsersDataList } from "../../utils/types";
 
 export function getDbUsers(): UsersDataList {
-  if (!existsSync("mockedDbData.json")) {
-    const newDataJson = JSON.stringify(mockedData, null, " ");
-    writeFileSync("/tmp/mockedDbData.json", newDataJson);
-  }
+  // if (!existsSync("/mockedDbData.json")) {
+  //   const newDataJson = JSON.stringify(mockedData, null, " ");
+  //   writeFileSync("/tmp/mockedDbData.json", newDataJson);
+  // }
 
-  const data = readFileSync("mockedDbData.json", "utf8");
+  const data = readFileSync("/mockedDbData.json", "utf8");
 
   const { users } = JSON.parse(data);
   return users;
