525422abd96cc49c4f12515cfa20cacf465c2a7d
Elena Ivanova <elena_ivanova@epam.com>
2023-03-31 18:06:58 +0300

    test commit

--- lib/dataHelpers/getDbUsers.ts
+++ lib/dataHelpers/getDbUsers.ts
@@ -6,7 +6,7 @@ import { UsersDataList } from "../../utils/types";
 export function getDbUsers(): UsersDataList {
   if (!existsSync("mockedDbData.json")) {
     const newDataJson = JSON.stringify(mockedData, null, " ");
-    writeFileSync("mockedDbData.json", newDataJson);
+    writeFileSync("/tmp/mockedDbData.json", newDataJson);
   }
 
   const data = readFileSync("mockedDbData.json", "utf8");
